34286d1d499042a525dcd304001019e95fa0d9e1
unknown <elsaied.samaka1997@gmail.com>
2023-02-17 07:23:19 +0200

    setup signin controller

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -28,6 +28,42 @@ const signup = async (req, res) => {
   }
 };
 
+// signin controller
+const signin = async ( req, res ) => {
+    try {
+    const existing_user= await User.findOne({
+     where: {
+     email: req.body.email,
+    },
+    } );
+        if ( !existing_user ) {
+            return res.status( 404 ).json( { message: "User Not found." } );
+        }
+        var passwordIsValid = bcrypt.compareSync(
+          req.body.password,
+          existing_user.password
+        );
+        if (!passwordIsValid) {
+          return res.status(401).send({
+            accessToken: null,
+            message: "Invalid Password!",
+          });
+        }
+        var token = jwt.sign( { id: existing_user.id }, config.secret, {
+          expiresIn: 86400, // 24 hours
+        } );
+        res.status( 200 ).send( {
+            id: existing_user.id,
+            firstname: existing_user.username,
+            email: existing_user.email,
+            accessToken: token,
+        } );
+    } catch ( error ) {
+        res.status( 500 ).send( { message: error.message } );
+    }
+}
+
 module.exports = {
   signup,
+  signin,
 };
--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -3,8 +3,6 @@ const router = express.Router();
 const authController = require("../controllers/auth.controller");
 
 router.post("/signup", authController.signup);
-router.get("/test", async (req, res) => {
-  res.send("signup");
-});
+router.post("/signin", authController.signin);
 
 module.exports = router;
